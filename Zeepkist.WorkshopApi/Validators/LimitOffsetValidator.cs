237ca5482d02103e72e07badc04eec5639302771
Christiaan Bloemendaal <github@tnrd.net>
2023-08-04 10:21:57 +0200

    feat: add default validator for limit offset

---
+++ Zeepkist.WorkshopApi/Validators/LimitOffsetValidator.cs
@@ -0,0 +1,12 @@
+﻿using TNRD.Zeepkist.WorkshopApi.RequestModels;
+
+namespace TNRD.Zeepkist.WorkshopApi.Validators;
+
+public class LimitOffsetValidator : Validator<LimitOffsetRequestModel>
+{
+    public LimitOffsetValidator()
+    {
+        RuleFor(x => x.Limit).GreaterThanOrEqualTo(0).LessThanOrEqualTo(100);
+        RuleFor(x => x.Offset).GreaterThanOrEqualTo(0);
+    }
+}
