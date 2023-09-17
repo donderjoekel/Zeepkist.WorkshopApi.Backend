49a246d1e9a996988a5ea49b4ad538470e5934b7
Christiaan Bloemendaal <github@tnrd.net>
2023-09-17 13:07:10 +0200

    feat: add last created and last updated endpoints

---
+++ Zeepkist.WorkshopApi.Backend/Endpoints/Levels/GetLastUpdated/Endpoint.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TNRD.Zeepkist.WorkshopApi.Backend.Db;
+using TNRD.Zeepkist.WorkshopApi.Backend.ResponseModels;
+
+namespace TNRD.Zeepkist.WorkshopApi.Backend.Endpoints.Levels.GetLastUpdated;
+
+public class Endpoint : EndpointWithoutRequest<IEnumerable<LevelResponseModel>>
+{
+    private readonly ZworpshopContext context;
+
+    public Endpoint(ZworpshopContext context)
+    {
+        this.context = context;
+    }
+
+    public override void Configure()
+    {
+        Get("levels/last-updated");
+        AllowAnonymous();
+        Description(b => b.ExcludeFromDescription());
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await SendOkAsync(await context.Levels.AsNoTracking()
+                .OrderByDescending(x => x.UpdatedAt)
+                .Take(1)
+                .Select(x => x.ToResponseModel())
+                .ToListAsync(ct),
+            ct);
+    }
+}
