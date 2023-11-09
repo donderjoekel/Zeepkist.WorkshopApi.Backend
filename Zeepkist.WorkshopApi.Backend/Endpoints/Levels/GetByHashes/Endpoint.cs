8ee9818893ea2d1688e75d0144c18eb49c96a8ff
Christiaan Bloemendaal <github@tnrd.net>
2023-11-09 17:10:39 +0100

    feat: add hashes endpoint

---
+++ Zeepkist.WorkshopApi.Backend/Endpoints/Levels/GetByHashes/Endpoint.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TNRD.Zeepkist.WorkshopApi.Backend.Db;
+using TNRD.Zeepkist.WorkshopApi.Backend.Db.Models;
+using TNRD.Zeepkist.WorkshopApi.Backend.ResponseModels;
+
+namespace TNRD.Zeepkist.WorkshopApi.Backend.Endpoints.Levels.GetByHashes;
+
+public class Endpoint : Endpoint<RequestModel, IEnumerable<LevelResponseModel>>
+{
+    private readonly ZworpshopContext context;
+
+    public Endpoint(ZworpshopContext context)
+    {
+        this.context = context;
+    }
+
+    public override void Configure()
+    {
+        Get("levels/hashes/{hashes}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(RequestModel req, CancellationToken ct)
+    {
+        string[] hashes = req.Hashes.Split(',');
+
+        IQueryable<LevelModel>? query = context.Levels.AsNoTracking()
+            .Where(x => hashes.Contains(x.FileHash));
+
+        if (!req.IncludeReplaced)
+        {
+            query = query.Where(x => x.ReplacedBy == null);
+        }
+
+        if (!req.IncludeDeleted)
+        {
+            query = query.Where(x => x.Deleted == false);
+        }
+
+        List<LevelModel> result = await query.ToListAsync(ct);
+
+        await SendOkAsync(result.Select(x => x.ToResponseModel()), ct);
+    }
+}
---
+++ Zeepkist.WorkshopApi.Backend/Endpoints/Levels/GetByHashes/Validator.cs
@@ -0,0 +1,9 @@
+﻿namespace TNRD.Zeepkist.WorkshopApi.Backend.Endpoints.Levels.GetByHashes;
+
+// public class Validator : Validator<RequestModel>
+// {
+//     public Validator()
+//     {
+//         RuleFor(x => x.Hashes).NotNull().NotEmpty();
+//     }
+// }
